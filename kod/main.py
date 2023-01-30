41506cf03579bf20a1def09aad007e91b77dfc93
Andrzej Klusiewicz <andrzejklusiewicz33@gmail.com>
2023-01-30 10:30:13 +0100

    zadanie 1

---
+++ kod/main.py
@@ -0,0 +1,28 @@
+# print('cośtam')
+# print("cośtam")
+# print("Mc'Donalds")
+# #przerwa do 10:22
+# owoc='banan'
+# kolor='żółty'
+# print(owoc)
+# print('owoc='+owoc)
+# print('owoc={}'.format(owoc))
+# zmienna='whatever'
+# print('owoc={} kolor={}'.format(owoc,kolor,zmienna))
+# print('owoc={} kolor={}'.format(owoc,kolor))
+# print('owoc='+owoc+" kolor="+kolor)
+# print(f'owoc={owoc} kolor={kolor}')
+# x=1.2
+# #print('x='+x)
+# print('x='+str(x))
+# print('x={}'.format(x))
+# print(f'x={x}')
+#
+# owoc=input('podaj ulubiony owoc:')
+# print(f'Twój ulubiony owoc to {owoc}')
+#
+# owoc=input('podaj ulubiony owoc:\n')
+# print(f'Twój ulubiony owoc to {owoc}')
+
+#1. Napisz program który przyjmie od użyszkodnika imię oraz nazwisko, a następnie
+#wypisze na konsoli komunikat typu "Witaj TwojeImie TwojeNazwisko!"
