edb5acd2ec920af46c0121631f9f5731fa108c0a
Andrzej Klusiewicz <andrzejklusiewicz33@gmail.com>
2023-01-30 15:35:24 +0100

    zadanie 18

--- kod/main.py
+++ kod/main.py
@@ -676,9 +676,26 @@
 # plt.grid()
 # plt.xlabel('oś x')
 # plt.ylabel('oś y')
+# plt.savefig('wykres.png')
 # plt.show()
 
 #17. Korzystając z list składanych wygeneruj listę 10 elementow której
 # każdy element również będzie listą.
 # Pierwszy element tej podlisty to numer potegi,
 # a drugi to wartosc tej potegi dla liczby 2
+#[ [x,x*100] for x .... ]
+#
+# lista=[ [e,pow(2,e)] for e in range(1,11)]
+# print(lista)
+#
+# for line in open('dane.csv',encoding='utf-8'):
+#     print(line.strip())
+#
+# line='2;Ferdynand;Kiepski;1.74;80'
+# list=line.split(';')
+# print(list)
+# print(list[3],float(list[3])/2)
+
+#18. Napisz program który z pliku dane.csv wyświetli powiekszone
+# imiona i nazwiska oraz wzrost i masę
+#list[2].lower()
