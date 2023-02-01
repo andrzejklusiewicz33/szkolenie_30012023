e61353b1c30af2162c2b6eb52078aa892e3d74a4
Andrzej Klusiewicz <andrzejklusiewicz33@gmail.com>
2023-02-01 09:39:51 +0100

    zadanie 38

--- kod/main.py
+++ kod/main.py
@@ -1616,6 +1616,34 @@
 #37.  Napisz funkcję która zwróci pod postacią listy krotek zawartość pliku
   # którego nazwę przekażemy przez pierwszy argument funkcji. Plik ma być otwarty z kodowaniem
   # podanym jako drugi argument funkcji. Jeśli kodowanie nie zostanie pdane ma przyjac utf-8.
-  #Kolumny mają być rozdzielane znakiem podanym jako trzeci argument funkcji, a jeśli nie zostanie podany to ma przyjąć średnik
+  #Kolumny mają być rozdzielane znakiem podanym jako trzeci argument funkcji,
+# a jeśli nie zostanie podany to ma przyjąć średnik
+
+# def get_csv(file_name,enc='utf-8',div=';'):
+#     print(f'file_name={file_name}')
+#     print(f'encoding={enc}')
+#     print(f'divisor={div}')
+#     return [line.strip().split(div) for line in open(file_name,encoding=enc)]
 
 
+# for e in get_csv('dane.csv'):
+#     print(e)
+#
+# for e in get_csv('dane.csv',enc="cp1252",div=' '):
+#     print(e)
+#
+# for e in get_csv('dane.csv',"cp1252",';'):
+#     print(e)
+#
+
+
+#38.   Napisz funkcję która bedzie w stanie przyjąć taką listę jaka jest zwracana
+ # przez funkcję z poprzedniego ćwiczenia. Funkcja ta ma przeiterować po otrzymanej
+ # liście i wyświetlić każdy element na konsoli. Odbierz dane z funkcji z ćwiczenia
+ # poprzedniego i przekaz do nowo powstalej funkcji.
+
+def get_csv(file_name,enc='utf-8',div=';'):
+    print(f'file_name={file_name}')
+    print(f'encoding={enc}')
+    print(f'divisor={div}')
+    return [line.strip().split(div) for line in open(file_name,encoding=enc)]
