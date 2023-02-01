9c2ac7e0f963ba3b4278c769134e5b031489f3ec
Andrzej Klusiewicz <andrzejklusiewicz33@gmail.com>
2023-02-01 13:32:33 +0100

    create.sql

--- kod/create.sql
+++ kod/create.sql
@@ -7,6 +7,6 @@ create table employees (
 );
 
 insert into employees(first_name,last_name,salary,description) values ('Jan','Kowalski',10000,'Opisa Jana Kowalskiego, bardzo długi bo będziemy tego potrzebować w aplikacji webowej');
-insert into employees(first_name,last_name,salary,description) values ('Stefan','Męczypupa',5000,'Opisa Stefana Meczypupy, bardzo długi bo będziemy tego potrzebować w aplikacji webowej');
+insert into employees(first_name,last_name,salary,description) values ('Stefan','Nowak',5000,'Opisa Stefana Nowaka, bardzo długi bo będziemy tego potrzebować w aplikacji webowej');
 insert into employees(first_name,last_name,salary,description) values ('Krzysztof','Testowy',8000,'Opisa Krzysztofa Testowego, bardzo długi bo będziemy tego potrzebować w aplikacji webowej');
 
