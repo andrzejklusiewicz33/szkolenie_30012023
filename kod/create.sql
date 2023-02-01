d5e2013a2d315acd261d212ce326bbc80e628b1c
Andrzej Klusiewicz <andrzejklusiewicz33@gmail.com>
2023-02-01 13:32:12 +0100

    create.sql

---
+++ kod/create.sql
@@ -0,0 +1,12 @@
+create table employees (
+  employee_id serial primary key,
+  first_name text not null,
+  last_name text not null,
+  salary numeric not null,
+  description text
+);
+
+insert into employees(first_name,last_name,salary,description) values ('Jan','Kowalski',10000,'Opisa Jana Kowalskiego, bardzo długi bo będziemy tego potrzebować w aplikacji webowej');
+insert into employees(first_name,last_name,salary,description) values ('Stefan','Męczypupa',5000,'Opisa Stefana Meczypupy, bardzo długi bo będziemy tego potrzebować w aplikacji webowej');
+insert into employees(first_name,last_name,salary,description) values ('Krzysztof','Testowy',8000,'Opisa Krzysztofa Testowego, bardzo długi bo będziemy tego potrzebować w aplikacji webowej');
+
