97ccb33831fd487e47274670513fd386157a1f19
Andrzej Klusiewicz <andrzejklusiewicz33@gmail.com>
2023-02-01 13:33:26 +0100

    create.sql

--- kod/create.sql
+++ kod/create.sql
@@ -10,3 +10,17 @@ insert into employees(first_name,last_name,salary,description) values ('Jan','Ko
 insert into employees(first_name,last_name,salary,description) values ('Stefan','Nowak',5000,'Opisa Stefana Nowaka, bardzo długi bo będziemy tego potrzebować w aplikacji webowej');
 insert into employees(first_name,last_name,salary,description) values ('Krzysztof','Testowy',8000,'Opisa Krzysztofa Testowego, bardzo długi bo będziemy tego potrzebować w aplikacji webowej');
 
+
+
+create table products(
+	product_id serial primary key,
+	product_name text not null,
+	price numeric not null default 0,
+	description text,
+	stock integer default 0
+);
+
+
+insert into products(product_name,price,description,stock) values ('Bulbulator',100,'Urządzenie które robi bul bul',32);
+insert into products(product_name,price,description,stock) values ('Półoś do Jelcza',1500,'Takie coś z takim czymś bez takiego czegoś',2);
+insert into products(product_name,price,description,stock) values ('Wihajster',300,'Z dzyndzlem albo bez',0);
